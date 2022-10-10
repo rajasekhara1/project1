7712652c90a020a4b4769d0b19822c6294f9d0ca
devrajasekhar1 <dev@gmail.com>
2022-10-10 13:33:46 +0530

    sharning the project by tl

---
+++ javaprogramm/src/javaprogramm/FirstProgramm.java
@@ -0,0 +1,10 @@
+package javaprogramm;
+
+public class FirstProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("first programm ");
+	}
+
+}
